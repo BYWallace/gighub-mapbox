de5e1ef847a65c314c9d3b1ae93fd6a6a87f6b50
BYWallace <bwallace90@gmail.com>
2014-08-05 12:05:01 -0400

    parsing response from seatgeek

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 var map = L.mapbox.map('map', 'examples.map-i80bb8p3');
 L.mapbox.featureLayer()
-  .loadURL('data/sweetgreen.geojson')
   .addTo(map);
 
-var locations = L.mapbox.featureLayer().addTo(map);
+var concerts = L.mapbox.featureLayer().addTo(map);
 
 $.ajax({
   url: "http://api.seatgeek.com/2/events?geoip=50.200.196.50&range=25mi&datetime_local=2014-08-07&taxonomies.name=concert",
@@ -15,43 +14,48 @@ $.ajax({
 var listings = document.getElementById('listing');
 
 function createMarkers(response) {
-  debugger;
-  locations.eachLayer(function(locale) {
-    // Parse into the geojson for each locale
-    var prop = locale.feature.properties;
+  var events = response.events;
 
-    var listing = listings.appendChild(document.createElement('div'));
-    listing.className = 'item';
+  for(var i=0; i < events.length; i++) {
+    var concert = events[i];
+    concerts.eachLayer(function(concert) {
+      debugger;
+      // Parse into the geojson for each locale
+      var prop = locale.feature.properties;
 
-    var link = listing.appendChild(document.createElement('a'));
-    link.href = '#';
-    link.className = 'title';
-    link.innerHTML = prop.address;
+      var listing = listings.appendChild(document.createElement('div'));
+      listing.className = 'item';
 
-    if (prop.crossStreet) {
-      link.innerHTML += '<br /><small>' + prop.crossStreet + '</small>';
-    }
+      var link = listing.appendChild(document.createElement('a'));
+      link.href = '#';
+      link.className = 'title';
+      link.innerHTML = prop.address;
 
-    var details = listing.appendChild(document.createElement('div'));
-    details.innerHTML = prop.city;
+      if (prop.crossStreet) {
+        link.innerHTML += '<br /><small>' + prop.crossStreet + '</small>';
+      }
 
-    if (prop.phone) {
-      details.innerHTML += ' &middot; ' + prop.phoneFormatted;
-    }
+      var details = listing.appendChild(document.createElement('div'));
+      details.innerHTML = prop.city;
 
-    link.onclick = function() {
-      // 1. Toggle an active class for `listing`. View the source in the demo link for example.
+      if (prop.phone) {
+        details.innerHTML += ' &middot; ' + prop.phoneFormatted;
+      }
 
-      // 2. When a menu item is clicked, animate the map to center its associated locale and open its popup.
-      map.setView(locale.getLatLng(), 16);
-      locale.openPopup();
-    };
+      link.onclick = function() {
+        // 1. Toggle an active class for `listing`. View the source in the demo link for example.
 
-    var popup = 'Sweetgreen';
-    locale.bindPopup(popup);
+        // 2. When a menu item is clicked, animate the map to center its associated locale and open its popup.
+        map.setView(locale.getLatLng(), 16);
+        locale.openPopup();
+      };
 
-    locale.on('click', function() {
-      map.setView(locale.getLatLng, 16);
+      var popup = 'Sweetgreen';
+      locale.bindPopup(popup);
+
+      locale.on('click', function() {
+        map.setView(locale.getLatLng, 16);
+      });
     });
-  });
+  }
 }
